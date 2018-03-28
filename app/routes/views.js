9b4a74a0bbaa05ab3f67ce3b56d93761641dab10
Frank Veloz <velozmontero@gmail.com>
2018-03-28 18:23:48 -0400

    improved root address redirect

--- app/routes/views.js
+++ app/routes/views.js
@@ -4,7 +4,12 @@ module.exports = function (app) {
   // HOME PAGE (with login links) ========
   // =====================================
   app.get('/', function (req, res) {
-    res.render('index.ejs'); // load the index.ejs file
+    if(req.user) {
+      res.redirect('/profile');
+    }
+    else {
+      res.render('index.ejs'); // load the index.ejs file
+    }
   });
 
   // =====================================
